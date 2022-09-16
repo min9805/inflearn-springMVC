1de29365f9a5f6756b36258f2faa0bba7273d1a9
min1925k@gmail.com <min1925k@gmail.com>
2022-09-16 18:21:58 +0900

    Member Administrate Application By JSP with with MVC Pattern. Apply FrontController Version 2
    - Seperate View.
    - Controller return View

---
+++ src/main/java/hello/servelt/web/frontcontroller/v2/controller/MemberListControllerV2.java
@@ -0,0 +1,25 @@
+package hello.servelt.web.frontcontroller.v2.controller;
+
+import hello.servelt.damain.member.Member;
+import hello.servelt.damain.member.MemberRepository;
+import hello.servelt.web.frontcontroller.v2.ControllerV2;
+import hello.servelt.web.frontcontroller.v2.MyView;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class MemberListControllerV2 implements ControllerV2 {
+	private MemberRepository memberRepository = MemberRepository.getInstance();
+
+	@Override
+	public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		List<Member> members = memberRepository.findAll();
+
+		request.setAttribute("members", members);
+
+		return new MyView("/WEB-INF/views/members.jsp");
+	}
+}
