72f409a94bc35daf8d997b5f9501f61ad4e7e09f
min1925k@gmail.com <min1925k@gmail.com>
2022-11-23 19:52:09 +0900

    Set response header
    - set response header by response.setHeader()
    - set content by response.setContentType(), setCharacterEncoding()
    - set cookie by Cookie class, response.addCookie()
    - set redirect by response.sendRedirect()

--- src/main/java/hello/servelt/basic/response/ResponseHeaderServlet.java
+++ src/main/java/hello/servelt/basic/response/ResponseHeaderServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+//set response header
 @WebServlet(name = "responseHeaderServlet", urlPatterns = "/response-header")
 public class ResponseHeaderServlet extends HttpServlet {
 	@Override
@@ -20,7 +21,7 @@ public class ResponseHeaderServlet extends HttpServlet {
 		response.setHeader("Content-Type", "text/plain;charset=utf-8");
 		response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
 		response.setHeader("Pragma", "no-cache");
-		response.setHeader("myHeader", "hello");
+		response.setHeader("my-Header", "hello");
 
 		//[Header 편의 메서드]
 		content(response);
@@ -34,6 +35,7 @@ public class ResponseHeaderServlet extends HttpServlet {
 
 	}
 
+	//response.sendRedirect를 사용해서 redirect
 	private void redirect(HttpServletResponse response) throws IOException {
 		//Status Code : 302
 		//Location : "/basic/hello-form.html"
@@ -43,6 +45,8 @@ public class ResponseHeaderServlet extends HttpServlet {
 		response.sendRedirect("/basic/hello-form.html");
 	}
 
+	//setHeader 로 세팅할 수 있지만 Cookie 라는 객체를 사용한다
+	//response.addCookie를 통해서 Cookie 객체를 삽입한다
 	private void cookie(HttpServletResponse response) {
 		//Set-Cookie : myCookie=good; Max-Age=600;
 
@@ -52,6 +56,8 @@ public class ResponseHeaderServlet extends HttpServlet {
 		response.addCookie(cookie);
 	}
 
+	//content type을 setHeader에서 직접 설정해주는 것이 아닌 기존 함수를 이용
+	//Content-Length는 임의로 정할 수도 있고, 생략 시 자동 계산 후 생성
 	private void content(HttpServletResponse response) {
 		//Content-Type : text/plain; charset=utf-8
 		//Content-Length : 2
